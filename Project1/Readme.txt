33cd0fcadd586318deba472f52cc1046cd9075e6
uDivy <upadhyaydivy10@gmail.com>
2022-10-01 22:08:20 -0400

    Project 1

---
+++ Project1/Readme.txt
@@ -0,0 +1,28 @@
+Name: Divya Upadhyay
+UFID: 2922-8848
+Email: divyaupadhyay@ufl.edu
+
+Environment Details:
+Language: Python v3.9
+IDE: PyCharm Community Edition 2022.1.1
+OS: Windows 11 Home Single Language
+
+The directory "Project1" contains the following files:
+ 1. Code for Multi Hashing Table is available in the file "multi_hashing_table.py".
+ 2. Code for Cuckoo Hash Table is available in the file "cuckoo_hash_table.py".
+ 3. Code for d-left Hash Table is available in the file "d-left_hash_table.py".
+ 4. Output for the Multi Hashing Table is available in the file "Output-MultiHashingTable.docx".
+ 5. Output for the Cuckoo Hash Table is available in the file "Output-CuckooHashTable.docx".
+ 6. Output for the d-left Hash Table is available in the file "Output-d-leftHashTable.docx".
+
+Steps to run the code:
+1. Open the python code in the PyCharm IDE.
+2. Use the following steps to build and activate the venv(virtual environment): https://www.cybrosys.com/blog/how-to-setup-virtual-environment-in-pycharm
+3. Once the virtual environment is created; you can activate the virtual environment by running the following cmd on the PyCharm terminal: 
+	a. You should be at the project location where the 'venv' is created.
+      b. Run the following in the terminal to activate the venv: <venv\Scripts\activate>
+      c. You can confirm if the following 'venv' is activated if you are able to see the 'venv' is added in front of the project path in the terminal:
+           (venv) D:\.....\Project1> 
+4. Once the 'venv' is activated you can install the required python dependencies using the following command: <pip install numpy>
+5. Now, the environment setup is completed and you can run all the above code one by one using: <python file_name.py> 
+	
---
+++ Project1/d-left_hash_table.py
@@ -0,0 +1,55 @@
+import numpy as np
+import math
+
+n = int(input("Number of table entries(n):\n"))
+m = int(input("Number of flows(m):\n"))
+d = int(input("Number of segments(d):\n"))
+
+print("The value of n, m and d are:",n, m, d)
+
+# Hash Function:: Generates index number randomly based on flowid between 0 and n
+def multiplication_hash(n, flowid, A=0.314):
+    h = math.floor(n*((flowid*A)%1))
+    return h
+
+# Function to generate the list of size 'm' of random flowid's in the range of [0 and m]
+def gen_flows(m):
+    flows = np.random.randint(1, m+1, m)
+    return flows
+
+# This is to generate d random numbers to generate multiple hash function using XOR
+randomnums = np.random.randint(1, m + 1, d)
+hash_table = [(0, 0)] * n
+flows = gen_flows(m)
+
+"""
+Function will check if the given flowid is present in the hash table, and in case if it is available it
+will increase the counter by 1 otherwise call the insert() function.
+"""
+def receive(flowid):
+    for i in range(d):
+        h = multiplication_hash(n, flowid ^ randomnums[i])
+        address = hash_table[h]
+        if address[0] == flowid:
+            hash_table[h] = (flowid, address[1] + 1)
+            return True
+    return insert(flowid)
+
+"""
+This will insert the new flowid prioritising the left most slot while insertion.
+"""
+def insert(flowid):
+    for i in range(d):
+        h = multiplication_hash(n, flowid ^ randomnums[i])
+        if hash_table[h][0] == 0:
+            hash_table[h] = (flowid, 1)
+            return True
+
+    return False
+
+for flowid in flows:
+    receive(flowid)
+
+print("The flowid of all the unique entries are: ",len([add[0] for add in hash_table if add[0] != 0]))
+print("The flowid of all the unique entries are: ",[add[0] for add in hash_table])
+
